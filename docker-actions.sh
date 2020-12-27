ed5d52013aba84a5f0248dc16207e2f1c4eed8cc
Martin Flores <martin.flores@bodybuilding.com>
2020-12-26 21:43:01 -0600

    fixing invocation to bash functions 5

--- docker-actions.sh
+++ docker-actions.sh
@@ -2,43 +2,55 @@
 
 set -euo pipefail
 
-dockerBuild() {
-  echo "== Docker Build =="
-  echo "Repo: $1"
-  echo "Version: $2"
-  mvn spring-boot:build-image -Dspring-boot.build-image.imageName=$1:$2 -Dmaven.test.skip=true
-}
+#dockerBuild() {
+#  echo "== Docker Build =="
+#  echo "Repo: $1"
+#  echo "Version: $2"
+#  mvn spring-boot:build-image -Dspring-boot.build-image.imageName=$1:$2 -Dmaven.test.skip=true
+#}
+#
+#dockerTag() {
+#  echo "== Docker Tag =="
+#  echo "Repo: $1"
+#  echo "Version: $2"
+#  docker tag $1:$2 $1:$2
+#}
+#
+#dockerPush() {
+#  echo "== Docker Push =="
+#  echo "Repo: $1"
+#  echo "Version: $2"
+#  docker push $1:$2
+#}
 
-dockerTag() {
-  echo "== Docker Tag =="
-  echo "Repo: $1"
-  echo "Version: $2"
-  docker tag $1:$2 $1:$2
-}
-
-dockerPush() {
-  echo "== Docker Push =="
-  echo "Repo: $1"
-  echo "Version: $2"
-  docker push $1:$2
-}
-
+ARTIFACT_ID=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.artifactId | grep -v '\[')
 POM_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\[')
 echo "== Docker =="
 echo "Repo: $1"
+echo "Artifact: $ARTIFACT_ID"
 echo "Version: $POM_VERSION"
 
-case $1 in
-build)
-  dockerBuild $2 $POM_VERSION
-  ;;
-tag)
-  dockerTag $2 $POM_VERSION
-  ;;
-push)
-  dockerPush $2 $POM_VERSION
-  ;;
-*)
-  echo "Unknown command"
-  ;;
-esac
+echo "== Docker Build =="
+mvn spring-boot:build-image -Dspring-boot.build-image.imageName=$1:$POM_VERSION -Dmaven.test.skip=true
+
+echo "== Docker Tag =="
+docker tag $ARTIFACT_ID:$POM_VERSION $1:$POM_VERSION
+
+echo "== Docker Push"
+docker push $1:$POM_VERSION
+
+
+#case $1 in
+#build)
+#  dockerBuild $2 $POM_VERSION
+#  ;;
+#tag)
+#  dockerTag $2 $POM_VERSION
+#  ;;
+#push)
+#  dockerPush $2 $POM_VERSION
+#  ;;
+#*)
+#  echo "Unknown command"
+#  ;;
+#esac
