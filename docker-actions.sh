a3df306100c896cdf8282ecf6bc79d993528f1c0
Martin Flores <martin.flores@bodybuilding.com>
2020-12-26 21:22:56 -0600

    fixing invocation to bash functions 3

--- docker-actions.sh
+++ docker-actions.sh
@@ -4,16 +4,22 @@ set -euo pipefail
 
 dockerBuild() {
   echo "== Docker Build =="
+  echo "Repo: $1"
+  echo "Version: $2"
   mvn spring-boot:build-image -Dspring-boot.build-image.imageName=$1:$2 -Dmaven.test.skip=true
 }
 
 dockerTag() {
   echo "== Docker Tag =="
+  echo "Repo: $1"
+  echo "Version: $2"
   docker tag $1:$2 $1:$2
 }
 
 dockerPush() {
   echo "== Docker Push =="
+  echo "Repo: $1"
+  echo "Version: $2"
   docker push $1:$2
 }
 
@@ -22,17 +28,17 @@ echo "== Docker =="
 echo "Repo: $1"
 echo "Version: $POM_VERSION"
 
-case $1 in
-build)
-  dockerBuild $2 $POM_VERSION
-  ;;
-tag)
-  dockerTag $2 $POM_VERSION
-  ;;
-push)
-  dockerPush $2 $POM_VERSION
-  ;;
-*)
-  echo "Unknown command"
-  ;;
-esac
+#case $1 in
+#build)
+#  dockerBuild $2 $POM_VERSION
+#  ;;
+#tag)
+#  dockerTag $2 $POM_VERSION
+#  ;;
+#push)
+#  dockerPush $2 $POM_VERSION
+#  ;;
+#*)
+#  echo "Unknown command"
+#  ;;
+#esac
