9f28e0230bd11b18b9aa01bfa0ed28b266647143
Martin Flores <martin.flores@bodybuilding.com>
2020-12-26 21:11:51 -0600

    fixing invocation to bash functions 2

--- docker-actions.sh
+++ docker-actions.sh
@@ -4,27 +4,24 @@ set -euo pipefail
 
 dockerBuild() {
   echo "== Docker Build =="
-  echo -e  "Repo: $1"
-  echo -e "Version: $POM_VERSION"
-  mvn spring-boot:build-image -Dspring-boot.build-image.imageName=$1:$POM_VERSION -Dmaven.test.skip=true
+  mvn spring-boot:build-image -Dspring-boot.build-image.imageName=$1:$2 -Dmaven.test.skip=true
 }
 
 dockerTag() {
   echo "== Docker Tag =="
-  echo -e  "Repo: $1"
-  echo -e "Version: $POM_VERSION"
-  docker tag $1:$POM_VERSION ${{ secrets.DOCKER_HUB_REPO }}:POM_VERSION
+  docker tag $1:$2 $1:$2
 }
 
 dockerPush() {
   echo "== Docker Push =="
-  echo -e  "Repo: $1"
-  echo -e "Version: $POM_VERSION"
-  docker push $1:POM_VERSION
+  docker push $1:$2
 }
 
 POM_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\[')
 echo "== Docker =="
+echo "Repo: $1"
+echo "Version: $POM_VERSION"
+
 case $1 in
 build)
   dockerBuild $2 $POM_VERSION
